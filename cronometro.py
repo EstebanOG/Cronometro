0181606fc9604a0c72ca1a0318209f789943773d
FelipeH22 <jfherrerap@correo.udistrital.edu.co>
2020-05-05 15:22:41 -0500

    Cambios en el orden

--- cronometro.py
+++ cronometro.py
@@ -1,15 +1,25 @@
 import time
-from crea_unidades import crea
+from crea_unidades import crea as instancias
 
-instancias = crea()
-instancias.crea_instancias()
-while True:
-    print(instancias.hh.valor_actual,":",instancias.mm.valor_actual,":",instancias.ss.valor_actual,":",instancias.ms.valor_actual)
-    time.sleep(0.1)
-    instancias.ms.valor_actual=instancias.ms.valor_actual+1
-    if instancias.ms.maximo_alcanzado()==True:
-        instancias.ss.valor_actual = instancias.ss.valor_actual+1 
-    if instancias.ss.maximo_alcanzado()==True:
-        instancias.mm.valor_actual = instancias.mm.valor_actual+1 
-    if instancias.mm.maximo_alcanzado()==True:
-        instancias.hh.valor_actual = instancias.hh.valor_actual 
+class cronometro:
+
+    stop=False        
+    def start(self):    
+        instancias.crea_instancias(instancias)          
+        while self.stop != True:
+            print(instancias.hh.valor_actual,":",instancias.mm.valor_actual,":",instancias.ss.valor_actual,":",instancias.ms.valor_actual)
+            time.sleep(0.1)
+            instancias.ms.valor_actual=instancias.ms.valor_actual+1
+            if instancias.ms.maximo_alcanzado()==True:
+                instancias.ss.valor_actual = instancias.ss.valor_actual+1 
+            if instancias.ss.maximo_alcanzado()==True:
+                instancias.mm.valor_actual = instancias.mm.valor_actual+1 
+            if instancias.mm.maximo_alcanzado()==True:
+                instancias.hh.valor_actual = instancias.hh.valor_actual+1 
+
+    def restart(self):
+        instancias.ms.valor_actual=0
+        instancias.ss.valor_actual=0
+        instancias.mm.valor_actual=0
+        instancias.hh.valor_actual=0
+        self.start()
---
+++ main.py
@@ -0,0 +1,7 @@
+from cronometro import cronometro as cronometro
+
+def main():
+    cronometro.start(cronometro)
+
+if __name__ == "__main__":
+    main()
